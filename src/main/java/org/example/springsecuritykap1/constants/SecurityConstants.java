fb85f9ca2b80844a48a9afaf220a4f2b31f65226
Jarl Tuxen <jart@kea.dk>
2025-11-04 09:40:11 +0100

    Tokengenerator added

---
+++ src/main/java/org/example/springsecuritykap1/constants/SecurityConstants.java
@@ -0,0 +1,8 @@
+package org.example.springsecuritykap1.constants;
+
+public class SecurityConstants {
+
+    public static final String JWT_KEY = "jxgEQeXHuPq8VdbyYfFNkANdudQ53Yn4";
+    public static final String JWT_HEADER = "Authorization";
+
+}
